56e87ea71ead6fee7a3e8dd61108acbc5559ae86
Pranav <pranavkjha111@gmail.com>
2023-04-08 00:00:19 +0530

    integrated lottie animation

--- app/src/main/java/com/griindset/xtraskill/LoginFragment.kt
+++ app/src/main/java/com/griindset/xtraskill/LoginFragment.kt
@@ -1,10 +1,12 @@
 package com.griindset.xtraskill
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.appcompat.widget.LinearLayoutCompat
 
 class LoginFragment : Fragment() {
@@ -16,6 +18,10 @@ class LoginFragment : Fragment() {
         // Inflate the layout for this fragment
         var view =  inflater.inflate(R.layout.fragment_login, container, false)
         var signupBtn = view.findViewById<LinearLayoutCompat>(R.id.signupBtn)
+        var lgnBtn = view.findViewById<Button>(R.id.lgn_btn)
+        lgnBtn.setOnClickListener {
+            startActivity(Intent(context,MainActivity::class.java))
+        }
         signupBtn.setOnClickListener {
             activity?.supportFragmentManager?.beginTransaction()?.setCustomAnimations(R.anim.slide_in,R.anim.fade_out,R.anim.fade_in,R.anim.slide_out)?.replace(R.id.lgn_frag,SignupFragment())?.addToBackStack(null)?.commit()
 
--- app/src/main/java/com/griindset/xtraskill/MainActivity.kt
+++ app/src/main/java/com/griindset/xtraskill/MainActivity.kt
@@ -2,10 +2,38 @@ package com.griindset.xtraskill
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var btmNav= findViewById<BottomNavigationView>(R.id.btmNvgtn)
+        btmNav.setOnItemSelectedListener { item->
+            when (item.itemId) {
+                R.id.nav_home -> {
+                    supportFragmentManager.beginTransaction().replace(R.id.mainFrame,HomeFragment()).commit()
+                }
+                R.id.nav_library -> {
+                    loadFragment(LibraryFragment())
+                }
+                R.id.nav_download -> {
+                    loadFragment(DownloadsFragment())
+                }
+                else -> {
+                    loadFragment(ProfileFragment())
+                }
+            }
+            true
+        }
+        btmNav.selectedItemId=R.id.nav_home
+
     }
+
+    private fun loadFragment(fragment:Fragment){
+        supportFragmentManager.beginTransaction().add(R.id.mainFrame,fragment).commit()
+    }
+
+
 }
